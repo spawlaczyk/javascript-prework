85964a28abd7000e27b511020f4488d6f4673658
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2021-12-03 10:39:35 +0100

    Add js to page

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = "kamień";
+let playerMove = "papier";
+
+printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
+printMessage("Zagrałeś " + playerMove + "! Jeśli mój ruch to kamień, to wygrywasz!");
