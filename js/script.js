401e560409a5d102a784b48f917878b5911b99a2
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2021-12-03 14:06:26 +0100

    Add script for playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Computer
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log("Wylosowana liczba to: " + randomNumber);
@@ -11,3 +12,20 @@ if(randomNumber == 2){
 }
 
 printMessage("Mój ruch to: " + computerMove);
+
+// Player
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce");
+
+console.log("Gracz wpisał: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if(playerInput == "1"){
+  playerMove = "kamień";
+} else if (playerInput == "2"){
+  playerMove = "papier";
+} else if (playerInput == "3"){
+  playerMove = "nożyce";
+}
+
+printMessage("Twój ruch to: " + playerMove);
